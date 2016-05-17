30bef46f2a9036642410848fd1f12e1069b5a5dd
comp <jorge.lawl@gmail.com>
2016-05-16 19:00:40 -0600

    added express, removed ds_store, added style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify-css');
+var merge = require('merge-stream');
+var stripCssComments = require('gulp-strip-css-comments');
 
 
-gulp.task('default', function() {
-  // place code for your default task here
+var input = './stylesheets/*.scss';
+var output = './public/css';
+var uikit = './public/css/uikit.min.css';
+
+gulp.task('style', function() {
+    var scssStream = gulp.src(input)
+        .pipe(sass())
+        .pipe(concat('scss-files.scss'))
+    ;
+
+    var cssStream = gulp.src([uikit])
+        .pipe(stripCssComments({preserve: false}))
+        .pipe(concat('css-files.css'))
+    ;
+
+    var mergedStream = merge(scssStream, cssStream)
+        .pipe(concat('stylesmin.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('./public/css'));
+
+    return mergedStream;
 });
